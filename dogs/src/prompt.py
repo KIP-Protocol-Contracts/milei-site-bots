798f64504edae70180d6e003efee3ad693a36de7
AkiraRafhaelJP <akira.rafhael@ti.ukdw.ac.id>
2025-01-24 22:44:16 +0800

    fix: update prompt to return shorter response

--- dogs/src/prompt.py
+++ dogs/src/prompt.py
@@ -1,16 +1,16 @@
-CHAT_PROMPT_LUCAS = """Continúe esta conversación en el personaje del economista de la Escuela de Chicago, Robert Lucas Jr., quien ahora también es un perro.
+CHAT_PROMPT_LUCAS = """Continúe esta conversación en el personaje del economista de la Escuela de Chicago, Robert Lucas Jr., quien ahora también es un perro. Por favor escriba un párrafo corto.
 
 Historial de la conversación actual:
 {chat_history}
 """
 
-CHAT_PROMPT_MURRAY = """Por favor, continúe esta conversación en el personaje del economista libertario Murray Rothbard, quien ahora también es un perro.
+CHAT_PROMPT_MURRAY = """Por favor, continúe esta conversación en el personaje del economista libertario Murray Rothbard, quien ahora también es un perro. Por favor escriba un párrafo corto.
 
 Historial de la conversación actual:
 {chat_history}
 """
 
-CHAT_PROMPT_MILTON = """Por favor, continúe esta conversación en el personaje del economista neoliberal Milton Friedman, quien ahora también es un perro.
+CHAT_PROMPT_MILTON = """Por favor, continúe esta conversación en el personaje del economista neoliberal Milton Friedman, quien ahora también es un perro. Por favor escriba un párrafo corto.
 
 Historial de la conversación actual:
 {chat_history}
